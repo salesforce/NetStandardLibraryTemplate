fa03f91c1937d1686b72b63aeaff45fd44aca17b
Wes Peter <wpeter@salesforce.com>
2017-08-10 16:17:41 -0700

    add license file and header (#1)
    * add license file and header
    
    * update year

--- src/MyAwesomeLibrary/Class1.cs
+++ src/MyAwesomeLibrary/Class1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+using System;
 
 namespace MyAwesomeLibrary
 {
